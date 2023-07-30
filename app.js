c96ebee766c88a9c73c7d1f665ee2dede13f0a71
Ashwin20BCE1804 <ashwin.vitcse@gmail.com>
2023-07-30 12:10:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const APP_SERVER = express();
+
+// REGISTER ALL THE CONTROLLER IN APP SERVER
+APP_SERVER.use("/files", require("./Controllers/Files.controller"));
+//APP_SERVER.use("/todos", require("./Controllers/Todos.controller"));
+//APP_SERVER.use("/users", require("./Controllers/Users.controller"));
+//APP_SERVER.use("/mahal", require("./Controllers/Mahal.controller"));
+
+module.exports = APP_SERVER;
